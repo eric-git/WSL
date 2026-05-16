26c319c9ac612b7aa5bae13d03f620ed777de2ae
Eric Wu <wu_yuqing@hotmail.com>
2026-05-16 10:03:33 +1000

    u

--- src/system-setup/debian-based/upgrade-system.sh
+++ src/system-setup/debian-based/upgrade-system.sh
@@ -4,4 +4,5 @@ echo "========== upgrade system =========="
 sudo apt-get -y upgrade
 sudo apt-get -y autoclean
 sudo apt-get -y autoremove --purge
+sudo journalctl --rotate
 sudo journalctl --vacuum-time=1s
